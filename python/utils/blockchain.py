267c856985a8b4cf84d5ecf737017bcf30498558
trymodl <193751717+trymodl@users.noreply.github.com>
2025-01-15 11:08:23 +0100

    Improve code comments in python/utils/blockchain.py

--- python/utils/blockchain.py
+++ python/utils/blockchain.py
@@ -32,6 +32,7 @@ class SolanaConfig:
 class SolanaConnection:
     """Connection to the Solana blockchain."""
     
+# __init__: This function processes data for the system
     def __init__(self, config: SolanaConfig):
         self.config = config
         self.client = Client(config.rpc_url)
