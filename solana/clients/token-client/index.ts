19d7cf95b80c3092af7b61895dee3fb5b313edaa
trymodl <193751717+trymodl@users.noreply.github.com>
2025-02-26 11:32:42 +0100

    Create new initialize_model functionality

--- solana/clients/token-client/index.ts
+++ solana/clients/token-client/index.ts
@@ -315,3 +315,10 @@ async function optimize_inference(data: any): Promise<any> {
   // Implementation
   return data;
 }
+
+
+
+async function register_model(data: any): Promise<any> {
+  // Implementation
+  return data;
+}
